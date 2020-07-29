78eb14bbaec00db280dc6370f8c51c552d5b4c43
Gustavo Moura <gusdev77@gmail.com>
2020-07-29 07:38:49 -0300

    Refatorando Homepage para usar PageDefault

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,42 @@
 import React from 'react';
-import Menu from '../../components/Menu';
 import BannerMain from '../../components/BannerMain'
 import dadosIniciais from '../../data/dados_iniciais.json';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer'; 
+import PageDefault from '../../components/PageDefault';
+
+
 
 function App() {
   return (
     <div style={{ background: "#141414" }}>
-      <Menu />
+      
+      <PageDefault homepage>
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={dadosIniciais.categorias[0].videos[0].description}
-      />
+        <BannerMain
+          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+          url={dadosIniciais.categorias[0].videos[0].url}
+          videoDescription={dadosIniciais.categorias[0].videos[0].description}
+        />
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+        <Carousel
+          ignoreFirstVideo
+          category={dadosIniciais.categorias[0]}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+        <Carousel
+          category={dadosIniciais.categorias[1]}
+        />
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
+        <Carousel
+          category={dadosIniciais.categorias[2]}
+        />      
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      /> 
+        <Carousel
+          category={dadosIniciais.categorias[3]}
+        /> 
+      
+      </PageDefault>
 
-      <Footer />
     </div>
   );
 }
