ff87326b919090da2a0f7eceeef2a4e2b4aa7720
Gustavo Moura <gusdev77@gmail.com>
2020-07-30 07:05:10 -0300

    Update

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod -R 777 ./*
-rm yarn.lock src/App.test.js src/serviceWorker.js src/setupTests.js  src/logo.svg src/App.css
+rm yarn.lock src/App.test.js src/serviceWorker.js src/setupTests.js  src/logo.svg src/App.css src/App.js
 exit 0
