ddc67167ea553ceaa60a57a09833c0153e962a27
Gustavo Moura <gusdev77@gmail.com>
2020-08-04 09:12:19 -0300

    Refactoring with proper linting

--- src/pages/cadastro/Categoria/components/InputField/index.js
+++ src/pages/cadastro/Categoria/components/InputField/index.js
@@ -1,16 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { CategoryInput, Container } from './styles';
 
 function InputField(props) {
-    return (
-        <Container>
-            <CategoryInput { ...props } />
-            <label htmlFor={ props.name }>{ props.label }</label>
-            { props.requiredMessage && props.submitted && (
-                <span>{ props.requiredMessage }</span>
-            )}
-        </Container>
-    )
+  const { inputProps, labelProps } = props;
+
+  return (
+    <Container>
+      <CategoryInput
+        as={inputProps.as}
+        onChange={inputProps.onChange}
+        type={inputProps.type}
+        name={inputProps.name}
+        value={inputProps.value}
+        required={inputProps.required}
+        style={inputProps.style}
+      />
+      <label htmlFor={inputProps.name}>{ labelProps.label }</label>
+      { labelProps.requiredMessage && labelProps.submitted && (
+        <span>{ labelProps.requiredMessage }</span>
+      )}
+    </Container>
+  );
 }
+
+InputField.defaultProps = {
+  inputProps: PropTypes.shape({
+    as: 'input',
+    required: false,
+    style: {},
+  }),
+  labelProps: PropTypes.shape({
+    label: 'Campo de entrada',
+    requiredMessage: 'Este campo precisa estar preenchido',
+    submitted: true,
+  }),
+};
+
+InputField.propTypes = {
+  inputProps: PropTypes.shape({
+    as: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    required: PropTypes.bool,
+    style: PropTypes.object,
+  }),
+
+  labelProps: PropTypes.shape({
+    label: PropTypes.string,
+    requiredMessage: PropTypes.string,
+    submitted: PropTypes.bool,
+  }),
+};
 
 export default InputField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,114 +1,131 @@
 import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault'
+import PageDefault from '../../../components/PageDefault';
 import InputField from './components/InputField';
 import { Button } from './styles';
 import './index.css';
 
 const CadastrarCategoria = () => {
   const valoresIniciais = {
-        nome: '',
-        descricao: '',
-        cor: '#CFB53B',
-        codeSec: '',
-        categoriaEnviada: false
-      }
+    nome: '',
+    descricao: '',
+    cor: '#CFB53B',
+    codeSec: '',
+    categoriaEnviada: false,
+  };
 
-  const [categorias, setCategorias] = useState([])
+  const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
-  function setValue(key, value){
+  function setValue(key, value) {
     setValues({
-      ...values, 
-      [key]: value
-    })
+      ...values,
+      [key]: value,
+    });
   }
-  
+
   function handleChange(e) {
     setValue(e.target.getAttribute('name'), e.target.value);
   }
 
-  function formValidation(state){
-    const invalids = Object.keys(state).filter((key) => {
-      return state[key] === "";
-    });
-    
-    return (invalids.length === 0 ? true : false);
+  function formValidation(state) {
+    const invalids = Object.keys(state).filter((key) => state[key] === '');
+
+    return (invalids.length === 0);
   }
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
-    if(e.target.name === "delCategoria"){
+    if (e.target.name === 'delCategoria') {
       setValues(valoresIniciais);
       return;
     }
     const ValidForm = formValidation(values);
-    setValues({...values, categoriaEnviada: true});
+    setValues({ ...values, categoriaEnviada: true });
 
-    if (! ValidForm){
-      return false;
+    if (!ValidForm) {
+      return;
     }
 
     setCategorias([...categorias, values]);
     setValues(valoresIniciais);
   }
 
-  return(
+  return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
       <form onSubmit={handleSubmit} noValidate>
-        <InputField onChange={handleChange} {...{
-          label: "Nome",
-          type: "text",
-          name: "nome",
-          value: values.nome,
-          required: true,
-          requiredMessage: "Nome é obrigatório",
-          submitted: values.categoriaEnviada
-        }} />
+        <InputField
+          inputProps={{
+            onChange: handleChange,
+            type: 'text',
+            name: 'nome',
+            value: values.nome,
+            required: true,
+          }}
+          labelProps={{
+            label: 'Nome',
+            requiredMessage: 'Nome é obrigatório',
+            submitted: values.categoriaEnviada,
+          }}
+        />
 
-        <InputField onChange={handleChange} as="textarea" {...{
-          label: "Descrição",
-          type: "text",
-          name: "descricao",
-          value: values.descricao,
-          required: true,
-          requiredMessage: "Descrição é obrigatória",
-          submitted: values.categoriaEnviada,
-          style: { height: "20vh", borderTop: "solid 24px rgba(0,0,0,0.0)", paddingTop: "0" }
-        }} />
+        <InputField
+          inputProps={{
+            onChange: handleChange,
+            as: 'textarea',
+            type: 'text',
+            name: 'descricao',
+            value: values.descricao,
+            required: true,
+            style: { height: '20vh', borderTop: 'solid 24px rgba(0,0,0,0.0)', paddingTop: '0' },
+          }}
+          labelProps={{
+            label: 'Descrição',
+            requiredMessage: 'Descrição é obrigatória',
+            submitted: values.categoriaEnviada,
+          }}
+        />
 
-        <InputField onChange={handleChange} {...{
-          label: "Cor da categoria",
-          type: "color",
-          name: "cor",
-          value: values.cor
-        }} />
+        <InputField
+          inputProps={{
+            onChange: handleChange,
+            type: 'color',
+            name: 'cor',
+            value: values.cor,
+          }}
+          labelProps={{
+            label: 'Cor da categoria',
+          }}
+        />
 
-        <InputField onChange={handleChange} {...{
-          label: "Código de Segurança",
-          type: "text",
-          name: "codeSec",
-          value: values.codeSec,
-          required: true,
-          requiredMessage: "O Código de Segurança é obrigatório",
-          submitted: values.categoriaEnviada
-        }} />
+        <InputField
+          inputProps={{
+            onChange: handleChange,
+            type: 'text',
+            name: 'codeSec',
+            value: values.codeSec,
+            required: true,
+          }}
+          labelProps={{
+            label: 'Código de Segurança',
+            requiredMessage: 'O Código de Segurança é obrigatório',
+            submitted: values.categoriaEnviada,
+          }}
+        />
 
         <Button name="addCategoria">Salvar</Button>
         <Button name="delCategoria" onClick={handleSubmit} className="cleanButton">Limpar</Button>
       </form>
       <ul>
         {
-          categorias.map((categoria, i) => {
-            return (
-              <li key={i}>{categoria.nome}</li>
-            )
-          })
+          categorias.map((categoria, i) => (
+            <li key={i}>{categoria.nome}</li>
+          ))
         }
       </ul>
 
     </PageDefault>
-  )
-}
+  );
+};
 
 export default CadastrarCategoria;
